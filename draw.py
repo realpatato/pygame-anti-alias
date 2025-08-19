5389089778b0c0706f2acec663789cf5ccf0cb1f
realpatato <jason.t.richardii@gmail.com>
2025-08-19 13:18:18 -0400

    Things can now go off screen without error.

--- draw.py
+++ draw.py
@@ -11,7 +11,11 @@ def rect(surface, color, rect, border_width=0):
         #draws it
         for i in range(rect[2]):
             for k in range(rect[3]):
-                surf_array[i + rect[0]][k + rect[1]] = [color[0], color[1], color[2], 255]
+                #tries, so that indexerrors are caught
+                try:
+                    surf_array[i + rect[0]][k + rect[1]] = [color[0], color[1], color[2], 255]
+                except IndexError:
+                    pass
     #converts the array to a surface
     output_surface = sc.create_surface(surf_array)
     #outputs the surface
@@ -33,11 +37,19 @@ def line(surface, color, start_pos, end_pos, width: int=1):
                 for i in range(abs(start_pos[1] - end_pos[1]) + 1):
                     #checks if start pos is greater than end
                     if start_pos[1] > end_pos[1]:
-                        #changes y addition accordingly
-                        surf_array[start_pos[0] + width_begin][i + end_pos[1]] = [color[0], color[1], color[2], 255]
+                        #tries, so that indexerrors are caught
+                        try:
+                            #changes y addition accordingly
+                            surf_array[start_pos[0] + width_begin][i + end_pos[1]] = [color[0], color[1], color[2], 255]
+                        except IndexError:
+                            pass
                     else:
-                        #changes y addition accordingly
-                        surf_array[start_pos[0] + width_begin][i + start_pos[1]] = [color[0], color[1], color[2], 255]
+                        #tries, so that indexerrors are caught
+                        try:
+                            #changes y addition accordingly
+                            surf_array[start_pos[0] + width_begin][i + start_pos[1]] = [color[0], color[1], color[2], 255]
+                        except IndexError:
+                            pass
                 #shifts width begin over by one
                 width_begin += 1
         elif start_pos[1] == end_pos[1]:
@@ -47,11 +59,19 @@ def line(surface, color, start_pos, end_pos, width: int=1):
                 for i in range(abs(start_pos[0] - end_pos[0]) + 1):
                     #checks if start pos is greater than end
                     if start_pos[0] > end_pos[0]:
-                        #changes x addition accordingly
-                        surf_array[i + end_pos[0]][start_pos[1] + width_begin] = [color[0], color[1], color[2], 255]
+                        #tries, so that indexerrors are caught
+                        try:
+                            #changes x addition accordingly
+                            surf_array[i + end_pos[0]][start_pos[1] + width_begin] = [color[0], color[1], color[2], 255]
+                        except IndexError:
+                            pass
                     else:
-                        #changes x addition accordingly
-                        surf_array[i + start_pos[0]][start_pos[1] + width_begin] = [color[0], color[1], color[2], 255]
+                        #tries, so that indexerrors are caught
+                        try:
+                            #changes x addition accordingly
+                            surf_array[i + start_pos[0]][start_pos[1] + width_begin] = [color[0], color[1], color[2], 255]
+                        except IndexError:
+                            pass
                 #shifts width begin over by one
                 width_begin += 1
         #converts the array to a surface
